454734318d92e8b0741970f601e06288122b899b
Diego Huamani <dhuamanilu@unsa.edu.pe>
2024-05-21 18:21:14 -0500

    Se agrego comentarios y se empezo a ver las frecuencias de cada feature

--- app/main.py
+++ app/main.py
@@ -1,5 +1,18 @@
 import pandas as pd
 import numpy as np
 
+#Leemos los datos con pandas
 data=pd.read_excel('bd_alquiler.xlsx')
+#Mostramos 5 registros como ejemplo
 print(data.head())
+
+#Mirando informacion como el numero de filas y columnas de nuestra base de datos
+print(data.shape)
+#Informacion sobre los features, ademas el tipo de dato y el uso de memoria
+print(data.info())
+
+#Extrayendo frecuencias de ocurrencia de cada feature en todos los registros
+for column in data.columns:
+    print(data[column].value_counts())
+    print("*"*12)
+
