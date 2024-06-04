1c13ab06760980c1498cedaba116de8f39bd56ac
Diego Huamani <dhuamanilu@unsa.edu.pe>
2024-06-03 21:55:17 -0500

    Se uso un primer modelo (regresion lineal) para ver la eficiencia, r^2 score :0-7 y MSE=539345

--- app/main.py
+++ app/main.py
@@ -4,20 +4,20 @@ import numpy as np
 #Leemos los datos con pandas
 data=pd.read_excel('bd_alquiler.xlsx')
 #Mostramos 5 registros como ejemplo
-print(data.head())
+#print(data.head())
 
 #Mirando informacion como el numero de filas y columnas de nuestra base de datos
-print(data.shape)
+#print(data.shape)
 #Informacion sobre los features, ademas el tipo de dato y el uso de memoria
-print(data.info())
+#print(data.info())
 
 #Extrayendo frecuencias de ocurrencia de cada feature en todos los registros
-for column in data.columns:
-    print(data[column].value_counts())
-    print("*"*12)
+#for column in data.columns:
+    #print(data[column].value_counts())
+    #print("*"*12)
 
 #Verificando la cantidad de registros que tienen algun campo faltante
-print(data.isna().sum())
+#print(data.isna().sum())
 
 data=data.drop(columns=['Unnamed: 0', 'Piso de ubicación', 'Vista al exterior', 'Tipo de cambio',
                         'Alquiler mensual en dólares corrientes', 'Alquiler mensual en soles constantes de 2009', 'Tipo de cambio', 'IPC'])
@@ -27,8 +27,37 @@ data['Años de antigüedad'].fillna(data['Años de antigüedad'].median(), inpla
 data['Número de baños'].fillna(data['Número de baños'].median(), inplace=True)
 data['Número de garages'].fillna(data['Número de garages'].median(), inplace=True)
 
-print(data.describe())
+#print(data.describe())
 data.to_csv('dataset_final.csv')
+
+# Convertir variables categóricas en variables dummy
+data = pd.get_dummies(data, columns=['Distrito'], drop_first=True)
+# Identificar filas con valores NaN en cualquier columna
+# rows_with_nan = data[data.isnull().any(axis=1)]
+# print(rows_with_nan)
+# Eliminar filas con valores NaN
+data = data.dropna()
+
 # Separar características (X) y la variable objetivo (y)
 X = data.drop(columns=['Alquiler mensual en soles corrientes'])
 y = data['Alquiler mensual en soles corrientes']
+
+# Dividir los datos en conjuntos de entrenamiento y prueba
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Entrenar un modelo de regresión lineal (primera prueba para ver como se comporta la regresino lineal)
+from sklearn.linear_model import LinearRegression
+
+from sklearn.metrics import mean_squared_error
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Evaluar el modelo
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+score = model.score(X_test, y_test)
+print(f'R^2 score: {score}')
+print(f'Mean Squared Error: {mse}')
